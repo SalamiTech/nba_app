7afaaeebfa1ac9dc266d5971bad3ae6f23a79fb0
SalamiTech <salamigideon01@gmail.com>
2022-12-01 08:30:02 +0000

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import "package:flutter/material.dart";
+import 'package:http/http.dart' as http;
+import 'package:nba_app/model/team.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  List<Team> teams = [];
+
+  //get teams
+  Future getTeams() async {
+    var response = await http.get(Uri.https('balldontlie.io', 'api/v1/teams'));
+    var jsonData = jsonDecode(response.body);
+
+    for (var eachTeam in jsonData['data']) {
+      final team =
+          Team(abbreviation: eachTeam['abbreviation'], city: eachTeam['city']);
+      teams.add(team);
+    }
+
+    print(teams.length);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getTeams();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Teams',
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: SafeArea(
+        child: FutureBuilder(
+          future: getTeams(),
+          builder: (context, snapshot) {
+            // is it done loading? then show team data
+            if (snapshot.connectionState == ConnectionState.done) {
+              return ListView.builder(
+                itemCount: teams.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                      ),
+                      child: ListTile(
+                        title: Text(teams[index].abbreviation),
+                        subtitle: Text(teams[index].city),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+            // if it's loading, show loading circular indicator
+            else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/model/team.dart
@@ -0,0 +1,9 @@
+class Team {
+  final String abbreviation;
+  final String city;
+
+  Team({
+    required this.abbreviation, //LAL
+    required this.city, // Los Angeles
+  });
+}
